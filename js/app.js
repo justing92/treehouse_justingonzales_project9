f714f288ee0b6669984bddab1d4ac89449abc09f
Justin Gonzales <justing@chefsbest.com>
2022-12-01 17:36:23 -0800

    Version 5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-const hover1 = document.querySelectorAll('#bw');
-const hover2 = document.getElementById("projects");
+const hover1 = document.querySelectorAll('.bw');
 
-hover1.forEach(addEventListener('mouseover', (event) => {
+hover1.forEach(function(i) {
+    i.addEventListener('mouseover', (event) => {
     event.target.style.filter = "grayscale(80%)";
-}))
+    })
+}
+);
+
+// const hover2 = document.getElementById("projects");
